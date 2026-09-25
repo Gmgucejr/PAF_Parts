06aa5e2f0b8de9647443c0147a2d95b95274e019
Gmgucejr <90874846+Gmgucejr@users.noreply.github.com>
2026-09-26 04:35:13 +0800

    Create Streamlit app to display PAF proposed items
    Initial implementation of a Streamlit app to display PAF proposed items from a CSV file.

---
+++ pages/1_Proposal.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import streamlit as st
+
+
+# In[2]:
+
+
+paf3 = pd.read_csv('https://raw.githubusercontent.com/Gmgucejr/PAF_Parts/refs/heads/main/paf_prop.csv')
+
+
+# In[3]:
+
+
+st.write("## PAF Proposed Items")
+st.dataframe(paf3, use_container_width=True)
+
+
+# In[ ]:
+
+
+#get_ipython().system('jupyter nbconvert --to script --output-dir="C:\\Users\\212554084\\Downloads" Proposal.ipynb')
+
+
+# In[ ]:
+
+
+
+
